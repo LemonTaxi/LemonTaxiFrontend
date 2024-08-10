641d3f1382ffe437cb9cb79f59ed1e64f135d49c
Finn <finn@lemonbase.com>
2024-08-10 18:46:26 +0900

    feat: 지도 페이지 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
+import { Button } from 'antd';
+import Link from 'next/link';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Button>
+        <Link href="/map">지도</Link>
+      </Button>
+    </div>
+  );
 }
