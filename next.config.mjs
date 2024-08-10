296eb3e59b67eb9915fdc823d7097b5552e1df67
Finn <finn@lemonbase.com>
2024-08-10 18:46:12 +0900

    chore: antd 세팅

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,11 @@ const nextConfig = {
     });
     return config;
   },
+  swcMinify: true,
+  transpilePackages: ['antd', '@ant-design', 'rc-util', 'rc-pagination', 'rc-picker', 'rc-notification', 'rc-tooltip'],
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import GlobalStyle from '@/styles/GlobalStyle';
+import themeConfig from '@/styles/themeConfig';
+import { ConfigProvider } from 'antd';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <ConfigProvider theme={themeConfig}>
+        <Component {...pageProps} />
+      </ConfigProvider>
     </>
   );
 }
